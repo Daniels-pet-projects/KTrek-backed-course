445c6178da89e423a0b4cc7244940813863a3eac
Oleg <olegyesterdays@gmail.com>
2024-08-16 14:45:24 +0300

    fix CRUD and redis

--- dist/configs/database/database-config-postgresql.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.databaseConfigPostgresql = void 0;
-require("dotenv/config");
-exports.databaseConfigPostgresql = {
-    databaseURL: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?schema=public`
-};
